255ac7e215127d69ebfb4542019a33af3d0a2864
Raendy Andhika El-Vicar <raendy.elvicar@gmail.com>
2021-09-15 14:02:46 +0700

    add login feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import Login from "./Pages/Login";
+import React, { useState } from 'react';
+import { PrivateRoute } from './Components';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { history } from './Helpers';
+import { Login } from './Pages/Login';
+import { Dashboard } from './Pages/Dashboard';
+import { About } from './Pages/About';
+
 
 function App() {
+
   return (
     <div className="App">
-      <Login/>
+        <Router history={history}>
+          <Switch>
+                <Route path="/login" component={Login} />
+                <PrivateRoute exact path="/dashboard" component={Dashboard} />
+                <PrivateRoute exact path="/about" component={About} />
+                <Redirect from="*" to="/" />
+          </Switch>
+        </Router>
     </div>
   );
 }
