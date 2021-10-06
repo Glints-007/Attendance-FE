e80cca13f704502371e0017c775e0e60e7027955
Raendy Andhika El-Vicar <raendy.elvicar@gmail.com>
2021-10-06 20:09:29 +0700

    solving fika branch

--- src/Actions/adminAction.js
+++ src/Actions/adminAction.js
@@ -1,24 +1,20 @@
 import { faColumns } from "@fortawesome/free-solid-svg-icons";
 import TableComponent from '../Components/TableComponent';
 import axios from "axios";
+import { authHeader } from "../Helpers";
 
 export const GET_USERS_LIST = "GET_USERS_LIST";
 
 export const getUsersList = () => {
 
-  let user = JSON.parse(localStorage.getItem('user'));
-  let token = user && user.access_token ? user.access_token : '';
   const config = {
-    headers: {
-      "Content-type": "application/json",
-      "Authorization": "Bearer " + token,
-    },
+    headers: authHeader(),
   };
 
   return (dispatch) => {
     axios
       .get
-      ("https://floating-journey-97236.herokuapp.com/api/v1/users", config)
+      (`${process.env.REACT_APP_API_URL}/users`, config)
       .then(function (response) {
         dispatch({
           type: GET_USERS_LIST,
@@ -41,43 +37,35 @@ export const getUsersList = () => {
 };
 
 export const deleteUser = (uid) => {
+
   const config = {
-    headers: {
-      "Content-type": "application/json", 
-      "Authorization": "Bearer 125|ALFUiv47Lm48lqRnzLHuPDjiNvo4qV6d47VbEiuf",
-    },
+    headers: authHeader(),
   };
 
   return (dispatch) => {
     axios
       .delete(
-         "https://floating-journey-97236.herokuapp.com/api/v1/users/"+uid, config)
+        `${process.env.REACT_APP_API_URL}/users/${uid}`, config)
       .then(function (response) {
-        console.log(response);
-        
+        window.location.reload()
       })
       .catch(function (error) {
         console.log(error);
-        
       });
   };
 };
 
 export const verifyUser = (uid) => {
   const config = {
-    headers: {
-      "Content-type": "application/json", 
-      "Accept" : "application/json",
-      "Authorization": "Bearer 125|ALFUiv47Lm48lqRnzLHuPDjiNvo4qV6d47VbEiuf",
-    },
+    headers: authHeader(),
   };
 
   return (dispatch) => {
     axios
       .put(
-        `https://floating-journey-97236.herokuapp.com/api/v1/users/${uid}/verify`, null, config)
+        `${process.env.REACT_APP_API_URL}/users/${uid}/verify`, null, config)
       .then(function (response) {
-        console.log(response);
+        window.location.reload()
       })
       .catch(function (error) {
         console.log(error);
@@ -87,20 +75,15 @@ export const verifyUser = (uid) => {
 
 export const rejectUser = (uid) => {
   const config = {
-    headers: {
-      "Content-type": "application/json",
-      "Accept" : "application/json", 
-      
-      "Authorization": "Bearer 125|ALFUiv47Lm48lqRnzLHuPDjiNvo4qV6d47VbEiuf",
-    },
+    headers: authHeader(),
   };
 
   return (dispatch) => {
     axios
       .put(
-        `https://floating-journey-97236.herokuapp.com/api/v1/users/${uid}/reject`, null, config)
+        `${process.env.REACT_APP_API_URL}/users/${uid}/reject`, null, config)
       .then(function (response) {
-        console.log(response);
+        window.location.reload()
       })
       .catch(function (error) {
         console.log(error);
